9fd7ba76f76c6b8052d99d288727fb8a966c0981
HarishV14 <harishv@testpress.in>
2024-09-19 18:54:31 +0530

    Completed ajax topic of listing bookmarked images and liked functionality were added by ajax

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -24,8 +24,8 @@ urlpatterns = [
     path("account/", include("account.urls")),
     path("", RedirectView.as_view(url="account/", permanent=True)),
     path("social-auth/", include("social_django.urls", namespace="social")),
-    path("", RedirectView.as_view(url="account/", permanent=True)),
     path("images/", include("images.urls", namespace="images")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
